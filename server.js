0be4a72bb2c83ed26b3e63e2baed654aa90917d7
Ryan Andrade <andmryan@gmail.com>
2025-04-17 22:25:43 -0400

    Completed Set up / Missed staging a file.

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 const authController = require('./controllers/auth.js');
 const foodsController = require('./controllers/foods.js');
 
@@ -43,7 +46,9 @@ app.get('/vip-lounge', (req, res) => {
   }
 });
 
+app.use(passUserToView);
 app.use('/auth', authController);
+app.use(isSignedIn);
 app.use('/users/:userId/foods', foodsController);
 
 app.listen(port, () => {
