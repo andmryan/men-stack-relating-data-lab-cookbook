d160a654f64a46313074ba722bb1889511050a57
Ryan Andrade <andmryan@gmail.com>
2025-04-17 23:49:55 -0400

    Small percentage of work done. Can't get adding food for a user to work. Will sort out asap.

--- controllers/foods.js
+++ controllers/foods.js
@@ -10,4 +10,10 @@ router.get('/', (req, res) => {
     res.render('foods/index.ejs');
 });
 
+// route to get to and render the new page from a logged in user
+router.get("/users/:userId/foods/new", (req, res) => {
+    // render the page
+    res.render("foods/new.ejs");
+});
+
 module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,5 +1,36 @@
 const mongoose = require('mongoose');
 
+const foodSchema = new mongoose.Schema({
+  // YOU DO: Define properties of food schema
+  // brand
+  brand: {
+    type: String,
+    required: true,
+  },
+  // name
+  name: {
+    type: String,
+    required: true,
+  },
+  // quantity
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  // ripeness
+  isRipe: {
+    type: String,
+    enum: ["ripe", "unripe", "notApplicable"],
+    required: true,
+  },
+  // freshness
+  isExpired: {
+    type: String,
+    enum: ["expired", "unexpired"],
+    required: true,
+  },
+});
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -11,15 +42,6 @@ const userSchema = new mongoose.Schema({
   },
   // Embed the foodSchema in the userSchema
   pantry: [foodSchema],
-});
-
-const foodSchema = new mongoose.Schema({
-  // YOU DO: Define properties of food schema
-  // name
-  name: {
-    type: String,
-    required: true,
-  }
 });
 
 const User = mongoose.model('User', userSchema);
